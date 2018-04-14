73a8973aaf1114fe21f89df571a76518e045cf21
microsystemlepe <37802878+microsystemlepe@users.noreply.github.com>
2018-04-14 16:08:22 +0200

    Crear Repo

---
+++ Repositorio/plugin.video.msystemnet/default.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# Youtube Channel
+# (c) 2015 - Simple TechNerd
+# Based on code from youtube addon
+#------------------------------------------------------------
+
+import os
+import sys
+import plugintools
+import xbmc,xbmcaddon
+from addon.common.addon import Addon
+
+addonID = 'plugin.video.msystemnet'
+addon = Addon(addonID, sys.argv)
+local = xbmcaddon.Addon(id=addonID)
+icon = local.getAddonInfo('icon')
+
+
+channellist=[
+        ("MicroSystem NETWORK", "channel/UC0LdsMFyFC8No4FG9atGEYA", 'icon')
+]
+
+
+
+# Entry point
+def run():
+    plugintools.log("msystemnet.run")
+    
+    # Get params
+    params = plugintools.get_params()
+    
+    if params.get("action") is None:
+        main_list(params)
+    else:
+        action = params.get("action")
+        exec action+"(params)"
+    
+    plugintools.close_item_list()
+
+# Main menu
+def main_list(params):
+    plugintools.log("msystemnet.main_list "+repr(params))
+
+for name, id, icon in channellist:
+	plugintools.add_item(title=name,url="plugin://plugin.video.youtube/"+id+"/",thumbnail=icon,folder=True )
+
+
+
+run()
